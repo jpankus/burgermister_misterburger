e5396e83da5ed35553aab889b690f9b0efb1ad52
Jessica Pankus <jessica@Jessicas-MacBook-Pro.local>
2017-04-06 23:05:22 -0400

    add burger files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Cheese');
+INSERT INTO burgers (name) VALUES ('Black and Blue');
+INSERT INTO burgers (name, yummy) VALUES ('Sylvester', true);
+INSERT INTO burgers (name, yummy) VALUES ('Marilyn Manson', true);
+INSERT INTO burgers (name, yummy) VALUES ('Joe Biden', true);
+INSERT INTO burgers (name) VALUES ('Owens');
